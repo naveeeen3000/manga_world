a4c83659b03ad3271cf867ceb05e4de3377503e0
naveeeen3000 <naveeeen3000@gmail.com>
2022-04-27 11:43:16 +0530

    fixed searchBar

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -29,6 +29,85 @@ export default{
 </script>
 
 <style> 
+@import url('https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Anek+Telugu&display=swap');
+
+nav{
+  background-color: rgb(0, 191, 212);
+  display: flex;
+  flex-direction:row;
+  align-items:center;
+  justify-content:space-between;
+}
+nav div{
+  padding:0px;
+  color:white;
+}
+.accounts{
+  font-size: 20px;
+  font-family: 'Anek Telugu', sans-serif;
+  background-color: rgb(85, 85, 85);
+  padding:10px;
+}
+.accounts a{
+  padding-left: 20px;
+}
+nav a{
+  color:white;
+  text-decoration: none;
+}
+.heading{
+    font-size: 30px;
+    font-family: 'Fredoka One', cursive;
+    
+}
+.right{
+  display: flex;
+}
+.searchBar{
+  font-size: medium;
+  border:0px;
+  height: 3em;
+  border:1px solid black;
+  width:25em;
+
+}
+.results{
+  background-color: rgb(86, 86, 86);
+  position: absolute;
+  z-index: 1;
+  top: 50px;
+  /* display: flex; */
+  width: 25.1em;
+}
+.search-list{
+  padding: 0px;
+  margin:0px;
+  margin-top: 5px;
+  display:flex;
+  flex-direction: row;
+  list-style: none;
+}
+.search-poster{
+  margin-left: 4px;
+  margin-top: 2px;
+}
+.search-poster img{
+  border-radius: 50%;
+  margin-left: 5%;
+  height: 60px;
+  width: 60px;
+}
+.search-info{
+  display:flex;
+  flex-direction: column;
+  margin-left: 6px;
+  font-family:Verdana, Geneva, Tahoma, sans-serif;
+}
+.search-list:hover{
+  background-color: rgb(0, 229, 255);
+}
+
 
 </style>
 
